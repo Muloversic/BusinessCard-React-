454b0a94788d6dc390c570263c965e421ea29143
Oleh Nikulin <Oleh_Nikulin@epam.com>
2022-01-25 16:24:18 +0200

    created markup for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import myImg from '../img/me.jpg'
 export default function Header() {
 	return (
-		<header>
-			<img src={myImg}/>
+		<header className='header'>
+			<img className='header__img' src={myImg} alt='me'/>
+			<section className='header__info'>
+				<div className='header__info-me'>
+					<h2 className='header__person-name'>Oleh Nikulin</h2>
+					<h3 className='header__person-position'>Frontend developer</h3>
+					<a className='header__person-link' href='#'></a>
+				</div>
+				<div className='header__contacts'>
+					<button className='header__btn header__btn--email'>Email</button>
+					<button className='header__btn header__btn--linkedin'>LinkedIn</button>
+				</div>
+			</section>
 		</header>
 	)
 }
